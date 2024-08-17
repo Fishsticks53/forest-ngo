5627caf235b1dcd4fa06d7a68d70eccff0a897f5
Fishsticks53 <150262983+Fishsticks53@users.noreply.github.com>
2024-08-17 14:10:54 +0530

    Footer.js

--- src/files/Footer.js
+++ src/files/Footer.js
@@ -1,6 +1,6 @@
 function Footer(){
     return(
-        <div className="h-14 bg-black text-white text-2xl  flex flex-col justify-center"><center>@Copyrights reserved 201x-202x</center></div>
+        <div className="h-14 bg-black text-white text-2xl  flex flex-col justify-center"><center>@Copyrights website testing reserved 201x-202x</center></div>
     )
 };
 
